3ef91e47c27ee677d5c878ef9437c937e43a735b
newb7171 <rotheory13@gmail.com>
2026-06-22 18:16:53 +0800

    Update Build-turnip.sh

--- Build-turnip.sh
+++ Build-turnip.sh
@@ -70,6 +70,8 @@ export OBJDUMP=llvm-objdump
 export OBJCOPY=llvm-objcopy
 export LDFLAGS="-fuse-ld=lld"
 
+export PKG_CONFIG_SYSROOT=$sysroot
+
 echo "setting crossfiles and setting up mesa..."
 
 cat <<EOF > android-aarch64.txt
@@ -80,7 +82,7 @@ cpp = ['$ndk/aarch64-linux-android35-clang++', '--sysroot=$sysroot', '-fno-excep
 c_ld = '$ndk/ld.lld'
 cpp_ld = '$ndk/ld.lld'
 strip = '$ndk/llvm-strip'
-pkg-config = ['env', 'PKG_CONFIG_LIBDIR=$ndk/pkg-config', '/usr/bin/pkg-config']
+pkg-config = ['env', 'PKG_CONFIG_LIBDIR=$sysroot/usr/lib/pkgconfig', '/usr/bin/pkg-config']
 
 [built-in options]
 c_args = ['--sysroot=$sysroot']
