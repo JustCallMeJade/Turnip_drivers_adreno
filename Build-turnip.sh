68be8ea825e5366c634aefe52f3497d71ccf1b09
JustCallMeJade <rotheory13@gmail.com>
2026-06-26 18:34:59 +0800

    Update Build-turnip.sh

--- Build-turnip.sh
+++ Build-turnip.sh
@@ -3,10 +3,10 @@
 set -euo pipefail
 
 workdir="$(pwd)/turnip_workdir"
-ndk="$workdir/r29/toolchains/llvm/prebuilt/linux-x86_64/bin"
+ndk="$workdir/r29/toolchains/llvm/prebuilt/linux-x86_64/bin" #yes r29 is the directory
 sysroot="$workdir/r29/toolchains/llvm/prebuilt/linux-x86_64/sysroot"
 mesasrc="https://gitlab.freedesktop.org/mesa/mesa.git"
-BUILD_VERSION="26.2.0-V5.0"
+BUILD_VERSION="26.2.0-V5.0" # this script is always maintained just update when new version.
 PATCH_1="https://raw.githubusercontent.com/newb7171/Turnip_drivers_adreno/main/Gpu-Hacks.patch"
 PATCH_2="https://raw.githubusercontent.com/newb7171/Turnip_drivers_adreno/main/KGSL-hacks-whitebelyash.diff"
 PATCH_3="https://github.com/lfdevs/mesa-for-android-container/commit/0a60c9c4108200fda20016b594dcf8806f29a28e.diff"
@@ -15,7 +15,10 @@ PATCH_4="https://github.com/lfdevs/mesa-for-android-container/commit/4bae24252a3
 echo "Only works in debian Arm64!!! press Ctrl + C to exit"
 echo "Installing build dependencies..."
 
-sed -i '/^Types:/ s/deb$/deb deb-src/' /etc/apt/sources.list.d/debian.sources
+if [ -f /etc/apt/sources.list.d/debian.sources ]; then
+    # .sources format
+    if ! grep -q '^Types:.*deb-src' /etc/apt/sources.list.d/debian.sources; then
+    sed -i '/^Types:/ s/$/ deb-src/' /etc/apt/sources.list.d/debian.sources
 
 apt-get update
 apt-get build-dep mesa -y -qq > /dev/null 2>&1
@@ -119,7 +122,7 @@ meson setup build-android-aarch64 \
     -Dfreedreno-kmds=kgsl \
     -Degl=disabled \
     -Dandroid-strict=false \
-    -Dshader-cache=true
+    -Dshader-cache=enabled
 
 ninja -C build-android-aarch64 install
 
@@ -138,7 +141,7 @@ cat <<EOF > meta.json
   "author": "JustCallMeJade",
   "packageVersion": "1",
   "vendor": "Mesa3D",
-  "driverVersion": "Vulkan 1.4.335",
+  "driverVersion": "Vulkan 1.4.334",
   "minApi": 28,
   "libraryName": "vulkan.adreno.so"
 }
