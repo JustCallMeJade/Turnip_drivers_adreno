b038fe3f8663258f6054582317a33b2f8c43620a
JustCallMeJade <rotheory13@gmail.com>
2026-06-27 12:11:05 +0800

    Update Build-turnip.sh

--- Build-turnip.sh
+++ Build-turnip.sh
@@ -11,8 +11,6 @@ PATCH_1="https://raw.githubusercontent.com/newb7171/Turnip_drivers_adreno/main/G
 PATCH_2="https://raw.githubusercontent.com/newb7171/Turnip_drivers_adreno/main/KGSL-hacks-whitebelyash.diff"
 PATCH_3="https://github.com/lfdevs/mesa-for-android-container/commit/0a60c9c4108200fda20016b594dcf8806f29a28e.diff"
 PATCH_4="https://github.com/lfdevs/mesa-for-android-container/commit/4bae24252a344c47a2afcd0fbd238d83bbc29f46.diff"
-PATCH_5="https://raw.githubusercontent.com/JustCallMeJade/Turnip_drivers_adreno/main/experimental.diff"
-
 echo "Only works in debian Arm64!!! press Ctrl + C to exit"
 echo "Installing build dependencies..."
 
