d2765496f61720f0945d9c0a56f365eb4e1eb217
JustCallMeJade <rotheory13@gmail.com>
2026-07-06 21:07:41 +0800

    Update Build-turnip.sh

--- Build-turnip.sh
+++ Build-turnip.sh
@@ -56,7 +56,6 @@ export VERSION="$(cat $workdir/mesa/VERSION)"
 
 for patch in \
 "$PATCH_1" \
-"$PATCH_2" \
 "$PATCH_3" \
 "$PATCH_4" \
 "$PATCH_5" \
@@ -78,7 +77,6 @@ wget https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/35924.diff
 
 for patch in \
 Gpu-Hacks.patch \
-KGSL-hacks-whitebelyash.diff \
 0a60c9c4108200fda20016b594dcf8806f29a28e.diff \
 4bae24252a344c47a2afcd0fbd238d83bbc29f46.diff \
 6338905ad3e8767bf5e5b04ffbbc6c3d9ed3d8e2.patch \
