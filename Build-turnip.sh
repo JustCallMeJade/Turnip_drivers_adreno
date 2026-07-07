e77799a93fafc15d0f6a3f5d2b82066f18d6877a
JustCallMeJade <rotheory13@gmail.com>
2026-07-07 12:23:06 +0800

    Update Build-turnip.sh

--- Build-turnip.sh
+++ Build-turnip.sh
@@ -74,25 +74,25 @@ wget https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/35924.diff
 
 for patch in \
 Gpu-Hacks.patch \
-6338905ad3e8767bf5e5b04ffbbc6c3d9ed3d8e2.patch \
-40159.diff \
+39751.patch \
 42159.patch \
 42489.patch \
-39751.patch
+6338905ad3e8767bf5e5b04ffbbc6c3d9ed3d8e2.patch
 do
 echo "Applying $patch..."
 git apply "$patch"
 done
 
 for patch in \
+35924.diff \
 40159.diff \
 0a60c9c4108200fda20016b594dcf8806f29a28e.diff \
 4bae24252a344c47a2afcd0fbd238d83bbc29f46.diff
 do
+echo "Applying $patch..."
 patch -p1 -i "$patch"
 done
-
-# Git apply doesn't work well with diffs 
+# git apply doesn't work well with diffs 
 
 
 for patch in \
