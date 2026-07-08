5b909fd934e32c9c8902feb095585f07c4f17cf9
JustCallMeJade <rotheory13@gmail.com>
2026-07-08 19:13:55 +0800

    Update patch downloads and apply commands in script

--- Extras/virgl-winlator.sh
+++ Extras/virgl-winlator.sh
@@ -16,13 +16,11 @@ cd workdir
 
 cd mesa
 
-wget https://github.com/alexvorxx/Mesa-VirGL/commit/f6cc3760680ba706c06f54f4d45e7ac8930241da.patch
-wget https://github.com/alexvorxx/Mesa-VirGL/commit/9cc60fdb4ad6d29340d38c98f3e7b849e9249780.patch
-wget https://github.com/alexvorxx/Mesa-VirGL/commit/208a22f98af7f56c7d0026de74d814d1860cb385.patch
+wget https://github.com/alexvorxx/Mesa-VirGL/commit/6a734cc1e1c6565fe688d0d05d37ecc3b2f330d2
+wget https://github.com/alexvorxx/Mesa-VirGL/commit/e67a72f8691dd450a527ab262b676e6fb21ec602
 
-git apply --3way --whitespace=fix f6cc3760680ba706c06f54f4d45e7ac8930241da.patch
-git apply --3way --whitespace=fix 9cc60fdb4ad6d29340d38c98f3e7b849e9249780.patch
-git apply --3way --whitespace=fix 208a22f98af7f56c7d0026de74d814d1860cb385.patch
+git apply --3way --whitespace=fix 6a734cc1e1c6565fe688d0d05d37ecc3b2f330d2.patch
+git apply --3way --whitespace=fix e67a72f8691dd450a527ab262b676e6fb21ec602.patch
 
 meson setup build -Dplatforms=x11 -Dvulkan-drivers= -Dglx=xlib -Dllvm=disabled -Dgallium-drivers=virgl -Dopengl=true -Degl=disabled -Dc_args="-Wno-error"
 
