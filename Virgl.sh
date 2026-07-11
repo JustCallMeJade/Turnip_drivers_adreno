a72c5c954758387921ecaf43e402c1443be216d4
JustCallMeJade <rotheory13@gmail.com>
2026-07-11 17:26:55 +0800

    Refactor Virgl.sh for improved readability and structure

--- Virgl.sh
+++ Virgl.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
 
-set -uo pipefail
+set -euo pipefail
 
-workdir=$(pwd)/workdir
-install_dir=$workdir/install
+workdir="$(pwd)/workdir"
+install_dir="$workdir/install"
 
 shopt -s extglob
 
-mkdir -p $workdir && mkdir -p $install_dir && cd workdir
+mkdir -p "$workdir" "$install_dir"
+cd "$workdir"
 
 echo "Installing build deps.."
 
 dnf update -y > /dev/null 2>&1
 
-dnf builddep mesa -y > /dev/null 2>&1 && dnf install git cmake python3 wget -y > /dev/null 2>&1 && dnf install xcb-* -y > /dev/null 2>&1 && dnf install x11-* -y > /dev/null 2>&1
+dnf builddep mesa -y > /dev/null 2>&1
+dnf install git cmake python3 wget -y > /dev/null 2>&1
+dnf install xcb-* -y > /dev/null 2>&1
+dnf install x11-* -y > /dev/null 2>&1
 
 echo "Cloning mesa.."
 
@@ -32,25 +36,34 @@ python3 Xlib-VirGL.py
 echo "Compiling..."
 
 meson setup build \
---prefix "$install_dir" \
--Dplatforms=x11 \
--Dglx=xlib \
--Dvulkan-drivers= \
--Dgallium-va=disabled \
--Dbuildtype=release \
--Dllvm=disabled \
--Dgallium-drivers=virgl \
--Dshared-glapi=enabled \
--Dopengl=true \
--Degl=disabled \
--Dgles2=disabled \
--Dgles1=disabled
+    --prefix "$install_dir" \
+    -Dplatforms=x11 \
+    -Dglx=xlib \
+    -Dvulkan-drivers= \
+    -Dgallium-va=disabled \
+    -Dbuildtype=release \
+    -Dllvm=disabled \
+    -Dgallium-drivers=virgl \
+    -Dshared-glapi=enabled \
+    -Dopengl=true \
+    -Degl=disabled \
+    -Dgles2=disabled \
+    -Dgles1=disabled
 
 ninja -C build install
 
 echo "Packaging VirGL..."
 
-cd "$install_dir" && cd lib64 && rm -rf !(libGL.so|libGL.so.1|libGL.so.1.*) && cd .. && mv lib64 VirGL
+cd "$install_dir/lib64"
+
+rm -rf !(libGL.so|libGL.so.1|libGL.so.1.*)
+
+cd ..
+
+mv lib64 VirGL
+
+# Automatically detect the real libGL version
+LIBGL_REAL=$(basename VirGL/libGL.so.1.*)
 
 cat > profile.json <<EOF
 {
@@ -68,17 +81,17 @@ cat > profile.json <<EOF
       "target": "\${libdir}/libGL.so.1"
     },
     {
-      "source": "VirGL/libGL.so.1.5.0",
-      "target": "\${libdir}/libGL.so.1.5.0"
+      "source": "VirGL/$LIBGL_REAL",
+      "target": "\${libdir}/$LIBGL_REAL"
     }
   ]
 }
 EOF
 
-tar -cJf VirGL-$VERSION.tar.xz VirGL profile.json
+tar -cJf "VirGL-$VERSION.tar.xz" VirGL profile.json
 
-mv VirGL-$VERSION.tar.xz VirGL-$VERSION.wcp
+mv "VirGL-$VERSION.tar.xz" "VirGL-$VERSION.wcp"
 
-echo "Done!✅"
+echo "Done! ✅"
 
 exit 0
