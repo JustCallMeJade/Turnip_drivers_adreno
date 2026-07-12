96a84d08e105f8587052cdcf1cb5b90229ec04f8
JustCallMeJade <rotheory13@gmail.com>
2026-07-12 10:29:23 +0800

    Update Virgl-server.sh

--- Virgl-server.sh
+++ Virgl-server.sh
@@ -6,7 +6,7 @@ install_dir="$workdir/install-dir"
 mkdir -p "$workdir" && mkdir -p "$install_dir"
 cd "$workdir"
 alias install="dnf update -y && dnf builddep mesa -y && dnf install pkg-config git cmake gcc gcc-c++ meson ninja-build python3 libepoxy-devel libdrm-devel mesa-libgbm-devel libX11-devel vulkan-loader-devel libva-devel -y"
-alias compile="git clone --depth=1 https://gitlab.freedesktop.org/virgl/virglrenderer.git && cd virglrenderer && meson setup build -Dplatforms=glx && ninja -C build -j$(nproc) install"
+alias compile="git clone --depth=1 https://gitlab.freedesktop.org/virgl/virglrenderer.git && cd virglrenderer && meson setup build --prefix "$install_dir" -Dplatforms=glx && ninja -C build -j$(nproc) install"
 install > /dev/null 2>&1
 compile
 exit 0
