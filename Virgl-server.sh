939edeb52af5ba1a887652ea6d19bca971a7cd5a
JustCallMeJade <rotheory13@gmail.com>
2026-07-12 09:22:04 +0800

    Remove epoxy and update install alias command

--- Virgl-server.sh
+++ Virgl-server.sh
@@ -5,7 +5,7 @@ workdir="$(pwd)/workdir"
 install_dir="$workdir/install-dir"
 mkdir -p "$workdir" && mkdir -p "$install_dir"
 cd "$workdir"
-alias install="dnf update -y && dnf builddep mesa -y && dnf install pkg-config git cmake epoxy gcc gcc-c++ meson ninja-build python3 pkg-config libepoxy-devel libdrm-devel mesa-libgbm-devel libX11-devel vulkan-loader-devel libva-devel -y"
+alias install="dnf update -y && dnf builddep mesa -y && dnf install pkg-config git cmake gcc gcc-c++ meson ninja-build python3 pkg-config libepoxy-devel libdrm-devel mesa-libgbm-devel libX11-devel vulkan-loader-devel libva-devel -y"
 alias compile="git clone --depth=1 https://gitlab.freedesktop.org/virgl/virglrenderer.git && cd virglrenderer && meson setup build -Dplatforms=glx && ninja -C build -j$(nproc) install"
 install
 compile
