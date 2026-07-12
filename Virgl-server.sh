16ba181658e30ccab828a8374316146b1cf9027e
JustCallMeJade <rotheory13@gmail.com>
2026-07-12 08:36:06 +0800

    Add epoxy package to installation alias

--- Virgl-server.sh
+++ Virgl-server.sh
@@ -5,7 +5,7 @@ workdir="$(pwd)/workdir"
 install_dir="$workdir/install-dir"
 mkdir -p "$workdir" && mkdir -p "$install_dir"
 cd "$workdir"
-alias install="dnf update -y && dnf builddep mesa -y && dnf install pkg-config git cmake -y"
+alias install="dnf update -y && dnf builddep mesa -y && dnf install pkg-config git cmake epoxy -y"
 alias compile="git clone --depth=1 https://gitlab.freedesktop.org/virgl/virglrenderer.git && cd virglrenderer && meson setup build -Dplatforms=glx && ninja -C build -j$(nproc) install"
 install
 compile
