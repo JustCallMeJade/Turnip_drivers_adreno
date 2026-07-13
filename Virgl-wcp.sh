5024a1b109f636d38c65627878c0b7554e3e42b7
JustCallMeJade <rotheory13@gmail.com>
2026-07-13 16:27:07 +0800

    Update Virgl-wcp.sh

--- Virgl-wcp.sh
+++ Virgl-wcp.sh
@@ -52,7 +52,8 @@ meson setup build \
     -Degl=disabled \
     -Dgles2=disabled \
     -Dgles1=disabled \
-    -Dvideo-codecs=all
+    -Dvideo-codecs=all \
+    -Dstrip=true
 
 ninja -C build install
 
